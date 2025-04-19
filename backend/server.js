aed5f38659e44d8a280a47ae05bc5bf5d708ab9d
Luiz <luizcaetite@gmail.com>
2025-04-18 23:19:48 -0300

    feat(server): add cookie parser and protect movie and TV routes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import { protectRoute } from './middleware/protectRoute.js';
 
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
@@ -12,10 +14,11 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
     console.log('Server starded at http://localhost:'+PORT);
