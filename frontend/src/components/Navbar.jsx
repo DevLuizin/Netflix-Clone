dcf8c6b9551622ba174dd2efca2e5d16cae3987b
Luiz <luizcaetite@gmail.com>
2025-04-20 13:07:55 -0300

    feat(ui): add user avatar, logout, and search functionality to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { Search } from 'lucide-react';
+import { LogOut, Menu, Search } from 'lucide-react';
+import { useAuthStore } from '../store/authUser';
 
 const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const { user, logout } = useAuthStore();
 
     const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
@@ -27,6 +29,18 @@ const Navbar = () => {
                     <Link to={"/history"} className='hover:underline'>
                         Histórico de Pesquisa
                     </Link>
+                </div>
+            </div>
+
+            <div className='flex gap-2 items-center z-50'>
+                < Link to={"/search"}>
+                    <Search className='size-6 cursor-pointer' />
+                </Link>
+                <img src={user.image} alt='Avatar' className='h-8 rounded cursor-pointer' />
+                <LogOut className='size-6 cursor-pointer' onClick={logout} />
+
+                <div className='sm:hidden'>
+                    <Menu className='size-6 cursor-pointer' onClick={toggleMobileMenu}/>
                 </div>
             </div>
 
