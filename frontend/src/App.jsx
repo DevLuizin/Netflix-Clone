03bdcb7b9979d38df22854335598425e096f5e8e
Luiz <luizcaetite@gmail.com>
2025-04-21 02:56:13 -0300

    feat(ui): add route for NotFoundPage to handle unmatched routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import SignUpPage from "./pages/SignUpPage";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
 import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/404";
 
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
@@ -42,6 +43,7 @@ function App() {
         <Route path="/watch/:id" element={user ? <WatchPage/> : <Navigate to={"/login"} />} />
         <Route path="/search" element={user ? <SearchPage/> : <Navigate to={"/login"} />} />
         <Route path="/history" element={user ? <SearchHistoryPage/> : <Navigate to={"/login"} />} />
+        <Route path="/*" element={<NotFoundPage />} />
 
       </Routes>
       <Footer />
