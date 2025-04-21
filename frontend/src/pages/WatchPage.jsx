6a0413b44e59fabda925c256d032a25a6ae5c417
Luiz <luizcaetite@gmail.com>
2025-04-20 22:08:32 -0300

    feat(ui): improve WatchPage with detailed content display and formatted dates

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -5,6 +5,16 @@ import { useContentStore } from '../store/content';
 import Navbar from '../components/Navbar';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import ReactPlayer from 'react-player';
+import { ORIGINAL_IMG_BASE_URL } from '../utils/constants';
+
+
+function formatReleaseDate(date) {
+    return new Date(date).toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+}
 
 const WatchPage = () => {
     const { id } = useParams();
@@ -110,6 +120,30 @@ const WatchPage = () => {
                             <span className='font-bold text-red-600'>{content?.title || content?.name}</span> 😢
                         </h2>
                     )}
+                </div>
+
+                {/* movie details */}
+                <div className='flex flex-col md:flex-row items-center justify-between gap-20 max-w-6xl mx-auto'>
+                    <div className='mb-4 md:mb-0'>
+
+                        <h2 className='text-5xl font-bold text-balance'>{content?.title || content?.name}</h2>
+
+                        <p className='mt-2 text-lg'>
+                            {formatReleaseDate(content?.release_date || content?.first_air_date)} | {" "}
+                            {content?.adult ? (
+                                <span className='mt-2 text-lg'>18+</span>
+                            ) : (
+                                <span className='text-green-600'>Livre</span>
+                            )}{" "}
+                        </p>
+
+                        <p className='mt-4 text-lg'>{content?.overview}</p>
+
+                    </div>
+
+                    <img src={ORIGINAL_IMG_BASE_URL + content?.poster_path} alt="Imagem do Poster" 
+                        className='max-h-[600px] rounded-md'
+                    />
                 </div>
             </div>
         </div>
