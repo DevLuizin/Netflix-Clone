8756d516bd7d2eed79edc864583344197e549380
Luiz <luizcaetite@gmail.com>
2025-04-21 02:51:24 -0300

    feat(utils): add formatDate function for custom date formatting

--- frontend/src/utils/dateFunction.js
+++ frontend/src/utils/dateFunction.js
@@ -5,3 +5,15 @@ export function formatReleaseDate(date) {
         day: "numeric",
     });
 }
+
+export function formatDate(dateString) {
+    const date = new Date(dateString);
+
+    const monthNames = ["Jan", "Fev", "Mar", "Abr", "Maio", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+    
+    const month = monthNames[date.getUTCMonth()];
+    const day = date.getUTCDate();
+    const year = date.getUTCFullYear();
+
+    return `${day} ${month} ${year}`;
+}
